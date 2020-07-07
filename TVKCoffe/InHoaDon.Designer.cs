0872ea23410cfde0ecc07fbd083baf1cee66d5be
tranvikhan <tranvikhan@gmail.com>
2020-07-07 12:41:44 +0700

    end

---
+++ TVKCoffe/InHoaDon.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace TVKCoffe
+{
+    partial class InHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InHoaDon));
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "TVKCoffe.Report.ReportHoaDon.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.ShowBackButton = false;
+            this.reportViewer1.ShowContextMenu = false;
+            this.reportViewer1.ShowCredentialPrompts = false;
+            this.reportViewer1.ShowDocumentMapButton = false;
+            this.reportViewer1.ShowExportButton = false;
+            this.reportViewer1.ShowFindControls = false;
+            this.reportViewer1.ShowPageNavigationControls = false;
+            this.reportViewer1.ShowParameterPrompts = false;
+            this.reportViewer1.ShowProgress = false;
+            this.reportViewer1.ShowPromptAreaButton = false;
+            this.reportViewer1.ShowRefreshButton = false;
+            this.reportViewer1.ShowStopButton = false;
+            this.reportViewer1.Size = new System.Drawing.Size(595, 624);
+            this.reportViewer1.TabIndex = 0;
+            this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
+            // 
+            // InHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 624);
+            this.Controls.Add(this.reportViewer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "InHoaDon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "In Hóa Đơn";
+            this.Load += new System.EventHandler(this.InHoaDon_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
---
+++ TVKCoffe/InHoaDon.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TVKCoffe.DAO;
+using TVKCoffe.DTO;
+
+namespace TVKCoffe
+{
+    public partial class InHoaDon : Form
+    {
+        private HoaDonDTO hoadon;
+        public InHoaDon(String maHD)
+        {
+            InitializeComponent();
+            hoadon = HoaDonDAO.Instance.getHoaDon(maHD);
+        }
+
+        private void InHoaDon_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void reportViewer1_Load(object sender, EventArgs e)
+        {
+            DataTable tb = new DataTable();
+            tb.Columns.Add("Stt", typeof(int));
+            tb.Columns.Add("TenSP", typeof(string));
+            tb.Columns.Add("SoLuong", typeof(int));
+            tb.Columns.Add("DonGia", typeof(string));
+            tb.Columns.Add("Tong", typeof(string));
+            List<ChiTietHoaDonDTO> list = ChiTietHoaDonDAO.Instance.getList(hoadon.MaHD);
+            int stt = 0;
+            decimal CongTienHang = 0;
+            foreach(ChiTietHoaDonDTO chitiet in list)
+            {
+                decimal Tong = (decimal)chitiet.SoLuong * chitiet.DonGia;
+                CongTienHang = CongTienHang + Tong;
+                tb.Rows.Add(++stt,chitiet.SanPham,chitiet.SoLuong,ConvertDTO.Instance.CurrencyFromDecimal(chitiet.DonGia), ConvertDTO.Instance.CurrencyFromDecimal(Tong));
+            }
+
+
+            ReportDataSource db = new ReportDataSource("DataSetHoaDon", tb);
+            ReportParameter[] pm = new ReportParameter[12];
+            pm[0] = new ReportParameter("Ngay", hoadon.NgayTao.ToString("dd/MM/yyyy"));
+            pm[1] = new ReportParameter("MaHD", hoadon.MaHD.ToString());
+            pm[2] = new ReportParameter("TenKH", hoadon.TenKH.ToString());
+            pm[3] = new ReportParameter("MaSoThue",hoadon.MaSoThue.ToString());
+            pm[4] = new ReportParameter("DiaChi", hoadon.DiaChi.ToString());
+            pm[5] = new ReportParameter("SoDienThoai", hoadon.SoDienThoai.ToString());
+            pm[6] = new ReportParameter("Thue", ConvertDTO.Instance.CurrencyFromDecimal(hoadon.Thue));
+            pm[7] = new ReportParameter("PhuThu", ConvertDTO.Instance.CurrencyFromDecimal(hoadon.PhuThu));
+            pm[8] = new ReportParameter("GiamGia", ConvertDTO.Instance.CurrencyFromDecimal(hoadon.GiamGia));
+            pm[9] = new ReportParameter("TongCong", ConvertDTO.Instance.CurrencyFromDecimal(hoadon.Tong));
+            pm[10] = new ReportParameter("PhanTramThue", Math.Round((hoadon.Thue / CongTienHang)*100,2).ToString());
+            pm[11] = new ReportParameter("CongTienHang", ConvertDTO.Instance.CurrencyFromDecimal(CongTienHang));
+
+            this.reportViewer1.LocalReport.SetParameters(pm);
+            this.reportViewer1.LocalReport.DataSources.Clear();
+            this.reportViewer1.LocalReport.DataSources.Add(db);
+            this.reportViewer1.ZoomMode = Microsoft.Reporting.WinForms.ZoomMode.PageWidth;
+            this.reportViewer1.RefreshReport();
+
+        }
+    }
+}
